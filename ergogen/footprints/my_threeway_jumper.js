139ed6fce52c20ebdf84e231c3fb865c13ab6de3
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-10-13 08:21:36 +0200

    improve madeline

--- ergogen/footprints/my_threeway_jumper.js
+++ ergogen/footprints/my_threeway_jumper.js
@@ -33,7 +33,7 @@ module.exports = {
         (fp_text reference "${p.ref}" (at 0 0) (layer F.SilkS) ${p.ref_hide} (effects (font (size 1.27 1.27) (thickness 0.15))))
         (fp_text value Jumper (at 0 -3) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
 
-        ${'' /* pins */}
+        ${'' /* pads */}
         (pad 1 smd rect (at -1.00076 0 ${p.r}) (size 0.635 1.143) (layers F.Cu F.Paste F.Mask)
         (clearance 0.1905) ${p.to_a})
         (pad 2 smd rect (at 0 0 ${p.r}) (size 0.635 1.143) (layers F.Cu F.Paste F.Mask)
@@ -48,9 +48,31 @@ module.exports = {
         (pad 6 smd rect (at 1.00076 0 ${p.r}) (size 0.635 1.143) (layers B.Cu B.Paste B.Mask)
         (clearance 0.1905) ${p.to_b})
 
-        (pad 7 thru_hole circle (at -1.00076 0) (size 0.6 0.6) (drill 0.3) (layers *.Cu) (zone_connect 2) ${p.to_a})
-        (pad 8 thru_hole circle (at 0 0) (size 0.6 0.6) (drill 0.3) (layers *.Cu) (zone_connect 2) ${p.from})
-        (pad 9 thru_hole circle (at 1.00076 0) (size 0.6 0.6) (drill 0.3) (layers *.Cu) (zone_connect 2) ${p.to_b})
+        ${'' /* vias */}
+        (pad 7 thru_hole circle 
+          (at -1.00076 0) 
+          (size 0.6 0.6) 
+          (drill 0.3) 
+          (layers "*.Cu" "*.Mask") 
+          (remove_unused_layers no)
+          (zone_connect 2) 
+          ${p.to_a})
+        (pad 8 thru_hole circle 
+          (at 0 0) 
+          (size 0.6 0.6) 
+          (drill 0.3) 
+          (layers "*.Cu" "*.Mask") 
+          (remove_unused_layers no)
+          (zone_connect 2) 
+          ${p.from})
+        (pad 9 thru_hole circle 
+          (at 1.00076 0) 
+          (size 0.6 0.6) 
+          (drill 0.3) 
+          (layers "*.Cu" "*.Mask") 
+          (remove_unused_layers no)
+          (zone_connect 2) 
+          ${p.to_b})
       )
     `
     if (p.side == 'both') {
